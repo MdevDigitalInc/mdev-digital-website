732615ec7e31961a953f30778001aac1b0d45027
Lucas Moreira <lucas@moreiradevelopment.io>
2018-06-14 17:03:05 -0400

    Optimized animation and added arrow shimmer

--- src/components/home/hero-main.vue
+++ src/components/home/hero-main.vue
@@ -1,6 +1,11 @@
 <template>
-  <header class="mdev-hero-mask" data-main-hero >
-    <div class="mdev-main-hero" :style="heroStyles"></div>
+  <header class="mdev-main-header" >
+    <div class="mdev-hero-mask" data-main-hero >
+      <div class="mdev-main-hero" :style="heroStyles"></div>
+    </div>
+    <div class="mdev-header-arrow-mask">
+      <div class="mdev-main-header-arrow"></div>
+    </div>
   </header>
 </template>
 
@@ -35,26 +40,72 @@ export default {
 	/*-------------------------------------*/
 	/* HERO MAIN Component Styles
 	/--------------------------------------*/
+  @import '../../assets/styles/keyframes/hero-anim.scss';
+
+  // Containers
+  .mdev-main-header {
+    width: 100%;
+    position: relative;
+    overflow: visible;
+  }
+
   .mdev-main-hero {
     width: 100%;
     position: relative;
     overflow: hidden;
-    @include responsive-ratio(1920,1080);
+    @include responsive-ratio( 1920, 1080 );
     max-height: 70vh;
+    z-index: 1;
   }
 
+  // Masks
   .mdev-hero-mask {
     overflow: hidden;
     position: relative;
     max-height: 97vh;
-    clip-path: polygon(50% 0%, 100% 0, 100% 100%, 50% 100%, 0 100%, 0 0);
-    transition: opacity .8s, clip-path 1.2s;
+    transition: opacity .8s;
     opacity: 0;
   }
 
+  .mdev-header-arrow-mask {
+    position: absolute;
+    top: 8px;
+    padding-top: 50.25%;
+    width: 100%;
+    clip-path: $hero-polygon-end; /*../styles/keyframes/hero-anim.scss */
+    z-index: -1;
+  }
+
+  // Animation Vars
+  $mask-hero-anim-time: 1s;
+  $mask-anim-delay: .8s;
+  $mask-arrow-anim-time: 2.2s;
+
+  .mdev-main-header-arrow {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    padding-top: 80%;
+    background: linear-gradient(top, rgba(194,236,47,1) 0%, rgba(181,221,45,1) 7%, rgba(71,91,30,0) 87%, rgba(10,19,21,0) 100%);
+    opacity: 0;
+    background-position: 0% 25%;
+    transform: translate3d( 0, -480px, 0);
+    animation: hero-gradient infinite;
+    animation-duration: $mask-arrow-anim-time;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: forwards;
+    animation-delay: $mask-hero-anim-time + $mask-anim-delay;
+  }
+
   .--mask-active {
-    clip-path: polygon(50% 0%, 100% 0, 100% 90%, 50% 100%, 0 90%, 0 0);
+    /*clip-path: polygon(50% 0%, 100% 0, 100% 90%, 50% 100%, 0 90%, 0 0);i*/
     opacity: 1;
+    animation: hero-animation;
+    animation-duration: $mask-hero-anim-time;
+    animation-iteration-count: 1;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: forwards;
   }
 	/*--------------------------------------*/
 
