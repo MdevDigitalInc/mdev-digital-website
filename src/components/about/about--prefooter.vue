d0b6acde917ce0feed1a4781b209989d3de93d26
Lucas Moreira <lucas@moreiradevelopment.io>
2019-05-02 16:59:13 -0400

    Updated About Contactus button to go to top of contact page

--- src/components/about/about--prefooter.vue
+++ src/components/about/about--prefooter.vue
@@ -50,7 +50,7 @@ export default{
       // Button Data
       BtnData: {
         accessibility: 'Contact Us!',
-        btnRoute: '/contact#mainContent',
+        btnRoute: '/contact',
         btnClass: 'mdev-dark-btn'
       },
       // Links pulled from Flat File
